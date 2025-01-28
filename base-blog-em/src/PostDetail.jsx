7c5b12e39f6cdc77695aaff57eb5df3ea753ae63
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-01-28 21:08:11 +0530

    added comments using UseQuery

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,10 +1,17 @@
 import { fetchComments } from "./api";
 import "./PostDetail.css";
+import { useQuery } from "@tanstack/react-query";
 
 export function PostDetail({ post }) {
   // replace with useQuery
-  const data = [];
+  const { data, isLoading } = useQuery({
+    queryKey: ["comments"],
+    queryFn: fetchComments,
+  });
 
+  if (isLoading) {  
+    return <h3 className="loading">Loading comments...</h3>;
+  }
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
