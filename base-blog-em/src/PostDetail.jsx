9d4479b177404abe3be9178feeeba8469ca6132f
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-01-29 20:24:53 +0530

    added useQuery to PostDetails

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -5,17 +5,18 @@ import { useQuery } from "@tanstack/react-query";
 export function PostDetail({ post }) {
   // replace with useQuery
   const { data, isLoading } = useQuery({
-    queryKey: ["comments"],
-    queryFn: fetchComments,
+    queryKey: ["comments", post.id],
+    queryFn: ()=> fetchComments(post.id),
   });
 
-  if (isLoading) {  
+  if (isLoading) {
     return <h3 className="loading">Loading comments...</h3>;
   }
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <button>Delete</button>
+      <button>Update title</button>
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
