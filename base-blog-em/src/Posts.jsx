061b339fa83a8e868decc5e92d9bae2516d504ff
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-02-02 21:14:43 +0530

    added prefetch

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,20 +1,30 @@
-import { useState } from "react";
-import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 
 import { fetchPosts, deletePost, updatePost } from "./api";
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
+  const queryClient = useQueryClient();
 
   // replace with useQuery
   const { data, isLoading, isError, error } = useQuery({
-    queryKey: ["posts"],
-    queryFn: fetchPosts,
-    staleTime:2000, // expiry time for the data so, after 2 seconds the data will be expired
+    queryKey: ["posts", currentPage],
+    queryFn: () => fetchPosts(currentPage),
+    staleTime: 2000, // expiry time for the data so, after 2 seconds the data will be expired
   });
+  useEffect(() => {
+    if (currentPage < 11) {
+      const nextPage = currentPage + 1;
+      queryClient.prefetchQuery({
+        queryKey: ["posts", nextPage],
+        queryFn: () => fetchPosts(nextPage),
+      });
+    }
+  }, [currentPage, queryClient]);
   if (isLoading) {
     return <h1>Loading....</h1>;
   }
@@ -41,11 +51,21 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage < 1}
+          onClick={() => {
+            setCurrentPage((currentPage) => currentPage - 1);
+          }}
+        >
           Previous page
         </button>
         <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage >= data.length}
+          onClick={() => {
+            setCurrentPage((currentPage) => currentPage + 1);
+          }}
+        >
           Next page
         </button>
       </div>
